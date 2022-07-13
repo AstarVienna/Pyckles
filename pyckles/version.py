e7e529aff503b5eae49153c023b18643b5a8e2fe
Kieran <kdleschinski@gmail.com>
2022-07-13 12:43:28 +0200

    minor changes to version.py

---
+++ pyckles/version.py
@@ -0,0 +1,8 @@
+version = '0.2'
+date    = '2022-07-12 17:00:00 GMT'
+yaml_descriptions = """
+- version : 0.2
+  date : 2022-07-12
+  comment : Updated DLC server URL to new scopesim.univie.ac.at space
+  
+"""
--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Learn more: https://github.com/kennethreitz/setup.py
-"""Pyckles"""
+"""
+Pyckles: A wrapper for common astronomical spectral libraries
+=======================================================================
 
+How to compile and put these on pip::
+
+    $ python setup.py sdist bdist_wheel
+    $ twine upload dist/*
+
+"""
 
 from setuptools import setup, find_packages
+
+# Version number
+with open('pyckles/version.py') as f:
+    __version__ = f.readline().split("'")[1]
 
 with open('README.md') as f:
     __readme__ = f.read()
 
-with open('LICENSE') as f:
-    __license__ = f.read()
-
 setup(
     name='pyckles',
-    version='0.1',
+    version=__version__,
     description="Simple interface to the Pickles 1998 stellar spectra catalogue",
     long_description=__readme__,
     long_description_content_type='text/markdown',
